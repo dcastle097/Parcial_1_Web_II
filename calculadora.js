08ebaf0a77738e83cd8ee9a975f39079a345726c
RafaelGutierrezz <36318470+RafaelGutierrezz@users.noreply.github.com>
2019-09-06 13:54:11 -0500

    Funcion Borrar
    Las variables finalmente van a ser Display1 y Display2.
    
    Display1 = a los valores que entran desde los botones
    Display 2 = al dar click en algun boton con alguna operación el display 2 toma el valor del display 1. Display 1 se resetea para esperar la nueva entrada.

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,10 @@
-function testing() {
-    console.log("Estamos vivos");
-}
+	var borrar = false;
+
+	function validarDisplay1() {
+	  Display2 = document.f1.txtDisplay2.value;
+	  var expreg = /^([0-9]{1,5})+$/;
+	  if(expreg.test(Display2))
+		alert("la expresion es correcta");
+	  else
+	    alert("la expresion es incorrecta");
+			}
