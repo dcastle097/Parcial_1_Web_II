13a24e901451710ee1209e944df639e76f89f8e1
RafaelGutierrezz <36318470+RafaelGutierrezz@users.noreply.github.com>
2019-09-06 14:07:23 -0500

    Adiciono Función Escribir

--- calculadora.js
+++ calculadora.js
@@ -9,3 +9,22 @@
              	var res = Display2.substring(0,Display2.length-1);
              	document.f1.txtDisplay2.value = res;
             }
+				}
+   function escribir(n){
+		var Display2 = document.f1.txtDisplay2.value;
+		if (borrar) {
+			//alert("se borro");
+			document.f1.txtDisplay2.value="";
+			borrar = false;
+			document.f1.txtDisplay2.value = n;
+		}
+		else if (Display2 == "0" && n != "."){
+			cajao = Display2.replace("0", "")
+			document.f1.txtDisplay2.value = cajao + n;
+		}
+		else{
+			document.f1.txtDisplay2.value = Display2 + n;
+		}
+	}
+
+	
